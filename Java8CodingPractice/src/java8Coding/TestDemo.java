acc6dadf5759eaeffe025aa2775182411a0f34c9
chetanbawane26 <bawanechetan123@gmail.com>
2025-01-08 02:22:25 +0530

    update commit

--- Java8CodingPractice/src/java8Coding/TestDemo.java
+++ Java8CodingPractice/src/java8Coding/TestDemo.java
@@ -3,7 +3,9 @@ package java8Coding;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -11,13 +13,19 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		
-		List<Integer> list= Arrays.asList(55,88,48,78,4,5,8,11,9,89,17);
+		List<Integer> list= Arrays.asList(55,88,48,78,4,5,8,11,9,89,17,11,4);
 		
 		//list.stream().filter(t->t%2==0).forEach(t->System.out.println(t));
 		
 		List<String> l1=list.stream().map(t->t + "").filter(t->t.startsWith("1")).collect(Collectors.toList());
-		System.out.println(l1);
+		//System.out.println(l1);
 
+		// Find Duplicates 
+		Set<Integer> set= new HashSet<>();
+		List<Integer> l2=list.stream()
+			.filter(t-> !set.add(t))
+				.collect(Collectors.toList());
+		System.out.println(l2);
 	}
 
 }
